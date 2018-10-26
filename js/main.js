b6013958fa58481d639e2de5d7a1174b1a51cea7
AyaneMatsubara <ayataka912nerv@gmail.com>
2018-10-26 23:11:44 +0900

    あとちょっと

--- js/main.js
+++ js/main.js
@@ -17,12 +17,11 @@ $(function(){
 	
 	$('#navbar').click(function() {
 		$('.menu').addClass('menu__active');
+		$('.menu__cover').css('visibility','visible');
 	})
-	$('#close').click(function() {
+	$('#close, .menu__item, .menu__cover').click(function() {
 		$('.menu').removeClass('menu__active');
-	})
-	$('.menu__item').click(function() {
-		$('.menu').removeClass('menu__active');
+		$('.menu__cover').css('visibility','hidden');
 	})
 	
 	$(function() {
@@ -32,13 +31,7 @@ $(function(){
 	});
 	
 	$(function() {
-		let leftlogo = $(".left-logo");
-		leftlogo.click(function () {
-    	$('body, html').animate({ scrollTop: 0 }, 500);
-    		return false;
-  	});
-		let footer = $("footer");
-		footer.click(function () {
+		$(".left-logo footer").click(function () {
     	$('body, html').animate({ scrollTop: 0 }, 500);
     		return false;
   	});
@@ -47,20 +40,27 @@ $(function(){
 	var appear = true;
   var scroll_ = $('#scroll_');
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 4500) {
+    if($(this).scrollTop() > 4500) {
       if (appear) {
         appear = false;
-        scroll_.stop().animate({
-          'left': '-40px'
-        }, 300);
+        scroll_.stop().animate({'left': '-40px'}, 300);
       }
-    } else {
+    }else{
       if (appear == false) {
         appear = true;
-        scroll_.stop().animate({
-         	'left': '40px'
-        }, 300);
+        scroll_.stop().animate({'left': '40px'}, 300);
       }
     }
+  });
+	
+	$('a[href^="#"]').not('a[href^="#modal"]').click(function() {
+		var speed = 750; 
+		var href= $(this).attr("href");
+		var target = $(href == "#" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position}, speed, 'swing');
+		$('.menu').removeClass('menu__active');
+		$('.menu__cover').css('visibility','hidden');
+		return false;
   });
 });
